b96375173eb70075f5accd0884e8ad68eb99d297
Rok Rozman <rokrozman@Roks-MacBook-Pro.local>
2021-12-01 12:46:46 +0100

    Uvozil podatke

---
+++ podatki/podatki1.R
@@ -0,0 +1,4 @@
+
+library(readr)
+england_premier_league_players_2017_to_2018_stats <- read_csv("podatki/england-premier-league-players-2017-to-2018-stats.csv")
+glava = head(england_premier_league_players_2017_to_2018_stats)
